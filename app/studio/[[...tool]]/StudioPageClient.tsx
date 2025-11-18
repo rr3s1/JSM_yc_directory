99f3897789dfb4ffb109f00d7c880924c9413413
rsl-design <raressilviulazar@gmail.com>
2025-11-18 19:14:27 +0000

    012   Sanity Setup

---
+++ app/studio/[[...tool]]/StudioPageClient.tsx
@@ -0,0 +1,10 @@
+ 'use client'
+
+import { NextStudio } from 'next-sanity/studio'
+import config from '../../../sanity.config'
+
+export default function StudioPageClient() {
+  return <NextStudio config={config} />
+}
+
+
