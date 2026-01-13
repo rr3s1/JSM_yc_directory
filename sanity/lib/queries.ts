fe98c94084699f2bdeb33bcf0ebb50eb0f4b6b92
rslowp <raressilviulazar@gmail.com>
2026-01-13 11:03:47 +0000

    019   Startup details

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -34,3 +34,10 @@ export const STARTUP_BY_ID_QUERY =
   pitch,
 }`);
 
+// Query for startup views only
+export const STARTUP_VIEWS_QUERY = defineQuery(
+  `*[_type == "startup" && _id == $id][0]{
+    views
+  }`
+);
+
